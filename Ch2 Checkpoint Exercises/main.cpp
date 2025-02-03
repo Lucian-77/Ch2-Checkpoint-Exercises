f620c40eece0ef4dc03985c8b479ef3c34d72ef5
Lucian-77 <lucianshambhu@gmail.com>
2025-02-03 12:22:30 -0500

    Changed phone number

--- Ch2 Checkpoint Exercises/main.cpp
+++ Ch2 Checkpoint Exercises/main.cpp
@@ -5,7 +5,7 @@ int main()
 	string name, address, phone;
 	name = "Lucian McLaughlin";
 	address = "140 Cooley Road, Williamsburg, VA";
-	phone = "805-452-1236";
+	phone = "805-452-1237";
 	cout << name << endl;
 	cout << address << endl;
 	cout << phone << endl;
